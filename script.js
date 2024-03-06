2f0be5107fb3807ae5801f0adeba53337ad66e4d
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-06 22:55:53 +0545

    fixed minor bugs in the project

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const toggleMenu = () => {
-  const menu = document.querySelector(".menu-links"); //select the menu
-  const icon = document.querySelector(".hamburger-icon"); //select the icon
-
-  menu.classList.toggle("open"); //toggle the class open
-  icon.classList.toggle("open"); //toggle the class open
-};
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
 const darkModeToggle = document.getElementById("dark-mode-icon");
 
